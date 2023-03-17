73d53fe0f5a9fa3a9c173ae3550adcedc36d27de
Evgeny <69719971+prMaRKoFpr@users.noreply.github.com>
2023-03-17 21:21:26 +0300

    Global: Created React Project, Added React Auth application. Need update css, add post request

---
+++ ts_site/src/components/Auth.jsx
@@ -0,0 +1,22 @@
+import logo from './assets/logo.png';
+import './styles/Auth.scss';
+
+const Reg = () => {
+  return (
+    <div className="Reg">
+      <div className="header">
+        <img src={logo} alt='stepi' />
+      </div>
+      <div className="main_content">
+        <p>Вход:</p>
+        <form action="" className="auth_form">
+        <input type="text"  name='login' itemID='login' className='auth-input'/>
+        <input type="password" name='login' itemID='login' className='auth-input'/>
+        <button>Accept</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Reg;
