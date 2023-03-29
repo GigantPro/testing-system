deac20f408cbb48722e43aa18111ca8831a9d297
GigantPro <pochtagigantpro@gmail.com>
2023-03-29 18:23:52 +0300

    DevOps: Fix Dockerfiles and docker-compose

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,5 @@
-from typing import NoReturn
 import asyncio
+from typing import NoReturn
 
 from .config import config
 from .requests import init_requests
@@ -9,13 +9,13 @@ import uvicorn
 
 
 def main() -> NoReturn:
-    init_database()
+    asyncio.run(init_database())
     
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(init_requests())
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(init_requests())
     
     uvicorn.run(
-        "app:app",
+        "src.app:app",
         host=config.ip,
         port=config.port,
         log_level=config.log_level,
