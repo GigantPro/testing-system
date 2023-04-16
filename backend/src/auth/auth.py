b62295ccfd50a3c330f50965468336a10bd6a253
GigantPro <pochtagigantpro@gmail.com>
2023-04-16 19:24:16 +0300

    Feat: add TTL cookie to config

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -4,17 +4,20 @@ from fastapi_users.authentication import AuthenticationBackend, JWTStrategy
 from .config import config
 
 
-cookie_transport = CookieTransport(cookie_max_age=3600)
+cookie_transport = CookieTransport(
+    cookie_name='auth_key',
+    cookie_max_age=config.TTL_COOKIE_DAYS,
+)
 
 SECRET = config.SECRET
 
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+    return JWTStrategy(secret=SECRET, lifetime_seconds=config.TTL_COOKIE_DAYS)
 
 
 auth_backend = AuthenticationBackend(
-    name="jwt",
+    name="AuthBackend",
     transport=cookie_transport,
     get_strategy=get_jwt_strategy,
 )
