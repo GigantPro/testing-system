fe36cb844bf319219fb5e1ee543d5eea839f6d40
GigantPro <pochtagigantpro@gmail.com>
2023-04-18 12:57:26 +0300

    Fix: Fix error with roles

--- backend/src/auth/init_roles.py
+++ backend/src/auth/init_roles.py
@@ -34,6 +34,7 @@ __standart_roles = [
 async def init_roles() -> None:
     async with engine.connect() as connection:
         for role_ in __standart_roles:
-            if not await connection.execute(select(Role).where(Role.id == role_['id'])):
+            result = await connection.execute(select(Role).where(Role.id == role_['id']))
+            if not result.fetchone():
                 await connection.execute(insert(Role).values(**role_))
         await connection.commit()
