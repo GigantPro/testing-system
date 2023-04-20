de3674935ed8cbd103e7e154c384b2d762a30aea
GigantPro <pochtagigantpro@gmail.com>
2023-04-20 12:04:25 +0300

    Add get_user_by_id method

---
+++ backend/src/auth/funcstions.py
@@ -0,0 +1,36 @@
+import inspect as insp
+
+from sqlalchemy import select
+from sqlalchemy.inspection import inspect
+
+from .database import User, engine
+from .schemas import UserRead 
+
+
+async def _get_user_by_id(user_id: int) -> None | User:
+    async with engine.connect() as connection:
+        user = await connection.execute(
+            select(User) \
+            .where(User.id == user_id)
+        )
+        user = user.fetchone()
+
+        user_cfg = {}
+        columns = inspect(User).c.keys()
+        
+        for i in range(len(columns)):
+            user_cfg[columns[i]] = user[i]
+
+        return User(**user_cfg)
+
+async def _get_user_read_by_user(user: User) -> UserRead:
+    userread_cfg = {}
+
+    columns_user = list(inspect(User).c.keys())
+    columns_user_read = list(UserRead.__fields__.keys())
+
+    for id_ in range(len(columns_user)):
+        if columns_user[id_] in columns_user_read:
+            userread_cfg[columns_user[id_]] = user.__getattribute__(columns_user[id_])
+
+    return UserRead(**userread_cfg)
