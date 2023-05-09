e404b0df8be6a162f71ca89c1036bd8a62ec3e1b
GigantPro <pochtagigantpro@gmail.com>
2023-05-09 23:23:08 +0300

    Add normal registration module

--- backend/src/auth/upload.py
+++ backend/src/auth/upload.py
@@ -1,8 +1,12 @@
-import json
-from fastapi import APIRouter, UploadFile
+import base64
+from random import randint
+from datetime import datetime
+
+from fastapi import APIRouter, Form
 from fastapi.responses import JSONResponse
 
 from .auth import fastapi_users
+from ..config import config
 
 
 current_user = fastapi_users.current_user()
@@ -14,6 +18,11 @@ upload_router = APIRouter(prefix='/upload')
 
 
 @upload_router.post('/avatar')
-async def get_user_by_id(file: UploadFile) -> str:
-    print(file.size)
-    return '123'
+async def get_user_by_id(file_type: str = Form(...), filedata: str = Form(...)) -> str:
+    filedata = filedata.strip()
+    img_recovered = base64.b64decode(filedata)  # decode base64string
+    file_name = f'avatar_' \
+        f'{str(datetime.timestamp(datetime.now())).replace(".", "")}_{randint(10**10, 10**11 - 1)}.{file_type}'
+    with open(f'{config.static_files_path}/' + file_name, 'wb') as file_write:
+        file_write.write(img_recovered)
+    return f'/api/static/{file_name}'
--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,10 +1,11 @@
 import './styles/Footer.scss';
 
-
 export const Footer = () => {
     return (
         <div className='footer'>
-            <p className='main-footer-content'>Powered by <a href="https://xiver.ru/">Xiver</a> group</p>
+            <p className='main-footer-content'>
+                Powered by <a href='https://xiver.ru/'>Xiver</a> group
+            </p>
         </div>
     );
 };
