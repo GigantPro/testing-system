4f45d0a2d9c1192e13c71e3f14e30d9a3fe7e8e8
Gigant Pro <pochtagigantpro@gmail.com>
2023-07-12 15:51:49 +0300

    Fix middleware

--- frontend/middleware/logged_users_only.js
+++ frontend/middleware/logged_users_only.js
@@ -1,11 +1,12 @@
-export default defineNuxtRouteMiddleware((to, from) => { // Fix me: Если есть return, то всегда приходит к нему
-    const headers = useRequestHeaders(['auth_key'])
-    const { data: user_data, pending } = useFetch(
+export default defineNuxtRouteMiddleware(async (to, from) => { // Fix me: Если есть return, то всегда приходит к нему
+    const headers = useRequestHeaders()
+    const { data: user_data, pending } = await useFetch(
         "http://backend:5001/user/self/who_am_i",
         { headers: headers, server: true }
     );
     console.log(user_data.value);
-    if (!user_data.value) {
+    const user = user_data.value;
+    if (!user) {
         console.log('No');
         console.log(pending);
         return '/'
