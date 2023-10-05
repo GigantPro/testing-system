145c55ca861a4f76c3c352c2f0ecd14cbb163b18
GigantPro <pochtagigantpro@gmail.com>
2023-10-05 19:17:02 +0300

    Fix profile loading

--- frontend/pages/@[username].vue
+++ frontend/pages/@[username].vue
@@ -1,13 +1,19 @@
 <script setup>
-const router = useRouter()
-
 const route = useRoute()
-const headers = useRequestHeaders()
-const { data: user_data, error } = await useFetch(
-    "http://backend:5001/user/username/" + route.params.username,
-    { headers: headers, server: true }
+const { data: user_data } = await useAsyncData(
+    'username_' + route.params.username,
+    () => {
+        const headers = useRequestHeaders()
+
+        let api_url = ''
+        if (process.server) api_url = process.env.SSR_API_BASE_URL + "/user/username/"  + route.params.username
+        else api_url = "/api/user/username/"  + route.params.username
+        return $fetch( 
+            api_url,
+            { headers: headers },
+        )
+    },
 );
-const user_value = user_data.value;
 
 useHead({
     title: route.params.username + ' | Xiver education',
@@ -17,9 +23,9 @@ useHead({
 <template>
     <div v-if="user_data" class="mt-auto mb-auto ms-5 me-5">
         <div class="text-center" id="userData">
-            <img :src="user_value.ico_url" alt="Ico" class="rounded-4 img-thumbnail mx-auto d-block mb-3" width="200">
-            <p class="mb-0"><b>{{ user_value.name }} {{ user_value.surname }}</b></p>
-            <p>@{{ user_value.username }}</p>
+            <img :src="user_data.ico_url" alt="Ico" class="rounded-4 img-thumbnail mx-auto d-block mb-3" width="200">
+            <p class="mb-0"><b>{{ user_data.name }} {{ user_data.surname }}</b></p>
+            <p>@{{ user_data.username }}</p>
         </div>
         <hr>
         <div class="text-center" id="userStats">Какая-то статистика</div>
