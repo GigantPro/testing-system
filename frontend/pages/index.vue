a569b47e6cc77803a2eb1583a59cb0bec0b6d691
GigantPro <pochtagigantpro@gmail.com>
2023-10-05 19:37:56 +0300

    Change more courses btn color

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -8,7 +8,7 @@
         </div>
         <div class="login_registration text-center d-block align-items-center pt-2 pb-2">
             <p><u>Чтобы начать обучение, войдите в аккаунт</u></p>
-            <p id="welcome_login_btn"><NuxtLink to="/auth?auth=login" class="btn btn-primary rounded login_btn">Войти</NuxtLink></p> <!-- Fix me: Надо, чтобы кнопка была по размеру зарегистрироваться -->
+            <p id="welcome_login_btn"><NuxtLink to="/auth?auth=login" class="btn btn-secondary rounded login_btn">Войти</NuxtLink></p> <!-- Fix me: Надо, чтобы кнопка была по размеру зарегистрироваться -->
             <p id="welcome_registration_btn"><NuxtLink to="/auth?auth=registration" class="registration_btn">Зарегистрироваться</NuxtLink></p>
         </div>
         <div class="top_courses text-reset">
