ee1952c7cd992f3e76c79dbd6461236c29beee03
GigantPro <pochtagigantpro@gmail.com>
2023-10-08 18:09:26 +0300

    Add popular courses by logic

---
+++ backend/src/courses/get/popular_course_by.py
@@ -0,0 +1,42 @@
+from fastapi import Query
+from fastapi.responses import JSONResponse
+
+
+from ..router import courses_router
+from src.functions import get_top_of_courses_by
+from src.types import CourseUserReadModel
+from src.database import Course, json_array_length
+
+
+__all__ = ("popular_courses_by",)
+
+dict_ = {
+    'rating':        Course.rating,
+    'created_time':  Course.created_at,
+    'reviews_count': json_array_length(Course.reviews),
+    'passed_count':  json_array_length(Course.passed_id),
+    'passing_count': json_array_length(Course.passing_id),
+}
+
+@courses_router.get('/popular_by')
+async def popular_courses_by(
+    start_index: int = Query(
+        default=0,
+        title='Start inxed for popular courses',
+        description='Will return courses from this index',
+    ),
+    count: int = Query(
+        default=10,
+        title='Count of returned courses',
+        description='Count of returned courses. Must be <= 50 and > 0',
+    ),
+    by_: str = Query(
+        title='The parameter by which you need to get filtered snare',
+        description='Must be one of thees: [created_time, passed_count, passing_count, reviews_count, rating]',
+    ),
+) -> list[CourseUserReadModel]:
+    if count > 50 or count <= 0 or by_ not in dict_:
+        return JSONResponse({'message': 'error'}, 400)
+
+    top_courses = await get_top_of_courses_by(start_index, count, dict_[by_])
+    return [CourseUserReadModel.from_orm(course) for course in top_courses]
--- backend/src/functions/courses/__init__.py
+++ backend/src/functions/courses/__init__.py
@@ -1,8 +1,9 @@
 from .create_new_course import create_new_course
 from .get_course_by_id import get_course_by_id
-from .get_course_by_param_func import get_course_by_param
+from .get_course_by_param import get_course_by_param
 from .get_courses_by_role import get_courses_by_role
 from .get_top_of_courses import get_top_of_courses
+from .get_top_of_courses_by import get_top_of_courses_by
 
 __all__ = (
     "create_new_course",
@@ -10,4 +11,5 @@ __all__ = (
     "get_course_by_param",
     "get_courses_by_role",
     "get_top_of_courses",
+    "get_top_of_courses_by",
 )
