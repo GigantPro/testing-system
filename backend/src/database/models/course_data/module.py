4a5141335da45987a929d9a1e18bb915084b0a17
GigantPro <pochtagigantpro@gmail.com>
2023-11-12 18:37:59 +0300

    Add db logic for courses

--- backend/src/database/models/course_data/module.py
+++ backend/src/database/models/course_data/module.py
@@ -1,16 +1,18 @@
 from sqlalchemy import TIMESTAMP, Column, Integer, ForeignKey
 from sqlalchemy.sql import func
 
+from .task import Task
 from ...base import Base
-from .module import Module
+from ..course import Course
 
 
-__all__ = ("CourseData",)
+__all__ = ("Module",)
 
-class CourseData(Base):
-    __tablename__ = 'courses_data'
+class Module(Base):
+    __tablename__ = 'modules'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    course = Column(ForeignKey(Course.id))
     changed_time = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
     created_time = Column(TIMESTAMP, server_default=func.now(), nullable=False)
-    modules = Column(list[ForeignKey(Module.id)], default=[])
+    tasks = Column(list[ForeignKey(Task.id)], default=[])
---
+++ backend/src/database/models/course_data/task.py
@@ -0,0 +1,37 @@
+from sqlalchemy import TIMESTAMP, Column, Integer, ForeignKey, String
+from sqlalchemy.sql import func
+
+from ...base import Base
+from ..course import Course
+
+
+__all__ = ("Task",)
+
+class Task(Base):
+    __tablename__ = 'task'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    course = Column(ForeignKey(Course.id))
+    changed_time = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
+    created_time = Column(TIMESTAMP, server_default=func.now(), nullable=False)
+    type = Column(Integer, nullable=False)
+    """
+    0 - text
+    1 - video
+    2 - test
+    """
+    title = Column(String, nullable=True)
+    text = Column(String, nullable=True)
+    description = Column(String, nullable=True)
+    video_url = Column(String, nullable=True)
+    tests_type = Column(Integer, nullable=True)
+    """
+    0 - simple test
+    1 - test with randow input data
+    """
+    simple_test_data = Column(dict[str:str], nullable=True)
+    """
+    {'test_input': 'solution_output'}
+    """
+    solution = Column(String, nullable=True)
+    solution_for_testing = Column(String, nullable=True)
